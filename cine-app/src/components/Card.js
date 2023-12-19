320f5a9e3cb368a36867635552973d0a38013910
Test <adrienlegeleu@gmail.com>
2023-12-19 08:06:27 +0100

    appcine

--- cine-app/src/components/Card.js
+++ cine-app/src/components/Card.js
@@ -1,26 +1,105 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Card = ({ film }) => {
+  const dateFormat = (date) => {
+    let [yy, mm, jj] = date.split("-");
+    return [jj, mm, yy].join("/");
+  };
+
+  const GenreMovie = () => {
+    let genreArray = [];
+    for (let i = 0; i < film.genre_ids.length; i++) {
+      switch (film.genre_ids[i]) {
+        case 28:
+          genreArray.push("Action");
+          break;
+        case 12:
+          genreArray.push("Aventure");
+          break;
+        case 16:
+          genreArray.push("Animation");
+          break;
+        case 35:
+          genreArray.push("Comédie");
+          break;
+        case 80:
+          genreArray.push("Crime");
+          break;
+        case 99:
+          genreArray.push("Documentaire");
+          break;
+        case 18:
+          genreArray.push("Drame");
+          break;
+        case 10751:
+          genreArray.push("Famille");
+          break;
+        case 14:
+          genreArray.push("Fantaisie");
+          break;
+        case 36:
+          genreArray.push("Histoire");
+          break;
+        case 27:
+          genreArray.push("Horreur");
+          break;
+        case 10402:
+          genreArray.push("Musique");
+          break;
+        case 9648:
+          genreArray.push("Mystère");
+          break;
+        case 10749:
+          genreArray.push("Romance");
+          break;
+        case 878:
+          genreArray.push("Science-Fiction");
+          break;
+        case 10770:
+          genreArray.push("TV Film");
+          break;
+        case 53:
+          genreArray.push("Thriller");
+          break;
+        case 10752:
+          genreArray.push("Guerre");
+          break;
+        case 37:
+          genreArray.push("western");
+          break;
+        default:
+          break;
+      }
+    }
+    return genreArray.map((genre) => <li key={genre}>{genre}</li>);
+  };
   return (
     <li className="card">
       <div className="img">
         <img
-          src={"https://image.tmdb.org/t/p/original/" + film.backdrop_path}
+          src={
+            film.backdrop_path
+              ? "https://image.tmdb.org/t/p/original/" + film.backdrop_path
+              : "./img/poster.jpg"
+          }
           alt=""
         />
       </div>
       <h1>{film.title}</h1>
-      <p className="date">sorti le {film.release_date}</p>
+      <p className="date">sorti le {dateFormat(film.release_date)}</p>
       <p className="note">
         {film.vote_average}/10{" "}
-        <i class="fa-solid fa-star" style={{ color: "#b1f41f" }}></i>
+        <i className="fa-solid fa-star" style={{ color: "#b1f41f" }}></i>
       </p>
-      <div className="film-genre"></div>
+      <ul className="film-genre">{film.genre_ids ? GenreMovie() : null}</ul>
       <div className="synopsis">
         <div className="title">
           <h3>Synopsis</h3>
         </div>
         <p>{film.overview}</p>
+      </div>
+      <div className="button">
+        <button>coup de coeur</button>
       </div>
     </li>
   );
--- cine-app/src/components/Search.js
+++ cine-app/src/components/Search.js
@@ -1,40 +1,68 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
-import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils";
 
 const Search = () => {
   const [filmData, setFilmData] = useState([]);
+  const [inputText, setInputText] = useState("code");
 
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/search/movie?api_key=aaa9dd0754978abcfdface154eb3a4a0&query=code&language=fr-FR"
+        `https://api.themoviedb.org/3/search/movie?api_key=aaa9dd0754978abcfdface154eb3a4a0&query=${
+          inputText ? inputText : "code"
+        }&language=fr-FR`
       )
       .then((res) => setFilmData(res.data.results));
-  }, []);
+  }, [inputText]);
+
+  const [sortGoodBad, setSortGoodBad] = useState("");
 
   return (
     <div className="search">
       <div className="search-container">
         <div className="input-search">
-          <input type="text" placeholder="nom de film" />
+          <input
+            type="text"
+            placeholder="nom de film"
+            onChange={(e) => {
+              setInputText(e.target.value);
+            }}
+          />
           <input type="submit" value="Rechercher" />
         </div>
         <div className="rank">
-          <button>
-            top <i className="fa-solid fa-arrow-up-long"></i>
+          <button
+            onClick={() => {
+              setSortGoodBad("goodtobad");
+            }}
+          >
+            top
+            <i className="fa-solid fa-arrow-up-long"></i>
           </button>
-          <button>
-            flop <i className="fa-solid fa-arrow-down-long"></i>
+          <button
+            onClick={() => {
+              setSortGoodBad("badtogood");
+            }}
+          >
+            flop
+            <i className="fa-solid fa-arrow-down-long"></i>
           </button>
         </div>
       </div>
       <div className="cards-container">
         <ul>
-          {filmData.map((film) => (
-            <Card key={film.id} film={film} />
-          ))}
+          {filmData
+            .sort((a, b) => {
+              if (sortGoodBad == "goodtobad") {
+                return b.vote_average - a.vote_average;
+              } else if (sortGoodBad == "badtogood") {
+                return a.vote_average - b.vote_average;
+              }
+            })
+            .map((film) => (
+              <Card key={film.id} film={film} />
+            ))}
         </ul>
       </div>
     </div>
